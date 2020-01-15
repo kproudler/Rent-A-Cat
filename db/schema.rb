aeb29017bf328994b890143d6a7409a5ac4a50a8
kproudler <kevinproudler@gmail.com>
2020-01-15 08:52:52 -0500

    Fixed Column Name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_14_124754) do
+ActiveRecord::Schema.define(version: 2020_01_15_135033) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 2020_01_14_124754) do
     t.date "birth_date", null: false
     t.string "color", null: false
     t.string "name", null: false
-    t.string "sec", null: false
+    t.string "sex", null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
