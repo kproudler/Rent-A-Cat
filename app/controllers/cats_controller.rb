876c3cc84017d190a9a16d361dd5ad386bdfb1ff
kproudler <kevinproudler@gmail.com>
2020-01-19 20:36:40 -0500

    added edit and update methods

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -22,9 +22,35 @@ class CatsController < ApplicationController
     end
 
     def create
-        render json: "creating cat"
+        @cat = Cat.new(cat_params)
+
+        if @cat.save
+            redirect_to cat_url(@cat)
+        else
+            render :new
+        end
     end
 
+    def edit
+        @cat = Cat.find_by(id: params[:id])
+        render :edit
+    end
+
+    def update
+        @cat = Cat.find_by(id: params[:id])
+
+        if @cat.update_attributes(cat_params)
+            redirect_to cat_url(@cat)
+        else
+            render :edit
+        end
+    end
     
+    private
+
+    def cat_params
+        params.require(:cat).permit(:age, :birth_date, :color, :description, :name, :sex)
+    end
+
 
 end
